b82b12b4f74d8441abaacf201d39eb0d3cdfa95c
hermionewy <wuyan.sysu@gmail.com>
2019-06-29 10:44:43 -0400

    added checkboxes

--- src/js/card.js
+++ src/js/card.js
@@ -21,8 +21,12 @@ function showCard(cardID) {
 	$newCard.removeAttribute('hidden');
 	$newCard.removeAttribute('aria-hidden');
 	if(openPages.indexOf(cardID)<0){
-        addClass($newCard, 'slide-in')
-    }
+		addClass($newCard, 'slide-in')
+	}
 }
 
-export default { hideAllCards, showCard };
+function changeCardTitle(cardID, title) {
+	d3.select(cardID).select('.title__game').html(title);
+}
+
+export default { hideAllCards, showCard, changeCardTitle };
