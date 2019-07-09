64c2dbfcf695a2f71f55a858d859d847af39d2ce
hermionewy <wuyan.sysu@gmail.com>
2019-07-09 08:18:07 -0400

    update text, style

--- src/js/startScroll.js
+++ src/js/startScroll.js
@@ -9,9 +9,6 @@ function init() {
 		offset: 0.2,
 		enter: el => {
 			const index = +d3.select(el).attr('data-index');
-			d3.selectAll('.step').classed('is-active', false);
-			d3.select(`#step-${index}`)
-				.classed('is-active', true);
 			console.log('enter', index);
 			switchIndex( index );
 			// scene.index(index)();
@@ -21,9 +18,6 @@ function init() {
 		exit: el => {
 			let index = +d3.select(el).attr('data-index');
 			index = Math.max(0, index - 1);
-			d3.selectAll('.step').classed('is-active', false);
-			d3.select(`#step-${index}`)
-				.classed('is-active', true);
 			console.log('exit', index);
 			switchIndex( index );
 		}
@@ -32,9 +26,19 @@ function init() {
 
 
 function switchIndex(idx) {
+
+	d3.selectAll('.step').classed('is-active', false);
+	d3.select(`#step-${idx}`)
+		.classed('is-active', true);
+	d3.selectAll('.card-sci-start__p').classed('active', false);
+	d3.select(`#step-${idx}`).select('.card-sci-start__p').classed('active', true);
+
 	const imgPath = `assets/images/start/animation/glitche_face_v2_${  idx+3 }.jpg`;
 	d3.select('#start-1').select('.card-sci-img')
 		.attr('data-src', imgPath)
 		.attr('src', imgPath);
+
+	d3.select('start1-step')
+
 }
 export default { init }
